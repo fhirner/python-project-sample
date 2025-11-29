cb7641c4bf096e423fd4f4f38af9881648857440
Felix Hirner <mail@felix-hirner.de>
2025-11-29 02:26:51 +0100

    fix: resolve linting errors

--- greeters/hello.py
+++ greeters/hello.py
@@ -2,5 +2,5 @@ def say_hello() -> str:
     return "Hello from pyproject-test!"
 
 
-def say_Personalized_hello(name: str) -> str:
-    return f"Hello { name}"
+def say_personalized_hello(name: str) -> str:
+    return f"Hello {name}"
